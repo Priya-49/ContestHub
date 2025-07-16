c37eb2fbc8c10fb322cdbb345003065db097852a
Priya-49 <priyapragada09@gmail.com>
2025-07-16 13:46:38 +0530

    Refactor: Switch cron job and reminder script to CommonJS (.js)

--- jobs/cron-runner.ts
+++
@@ -1,8 +0,0 @@
-import cron from 'node-cron';
-import { checkAndSendReminders } from '../scripts/sendRemainders';
-
-cron.schedule('*/5 * * * *', async () => {
-  console.log("⏰ Running cron job...");
-  await checkAndSendReminders();
-});
-
